76456d22db2be0478fc0ddfe6102d48e641ef30f
jmot4 <jmota@gtech-services.com>
2025-02-25 15:42:14 -0600

    Se agrega consulta de autores en DataGridView

--- AccesoDatos/Datos.cs
+++ AccesoDatos/Datos.cs
@@ -4,17 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Data.SqlClient;
+using System.Data;
 
 namespace AccesoDatos
 {
     internal class Datos
     {
         String cadenaConexion = "Data Source=DESKTOP-H69TC9M\\SQLEXPRESS;" +
-            "integrated security=true;initial catalog=pubs";
+            "integrated security=true;initial catalog=pubs;encrypt=false";
         SqlConnection conexion;
 
-        private SqlConnection abrirConexion() {
-            try {
+        private SqlConnection abrirConexion()
+        {
+            try
+            {
                 conexion = new SqlConnection(cadenaConexion);
                 conexion.Open(); //Abrir conexion a BD
                 return conexion;
@@ -26,18 +29,37 @@ namespace AccesoDatos
             }
 
         }
-        public bool prueba() {
+        public bool prueba()
+        {
             try
             {
                 abrirConexion();
                 return true;
             }
-            catch   (Exception ex)
+            catch (Exception ex)
             {
-              return false;
+                return false;
             }
-          
+
         }
-            
+
+
+        public DataSet Consulta(string consulta)
+        {
+            try
+            {
+                DataSet ds = new DataSet();
+                SqlDataAdapter da = new SqlDataAdapter(consulta, abrirConexion());
+                da.Fill(ds);
+                return ds;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                return null;
+            }
+
+
+        }
     }
 }
---
+++ AccesoDatos/frmAuthors.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace AccesoDatos
+{
+    partial class frmAuthors
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dgvAuthors = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)dgvAuthors).BeginInit();
+            SuspendLayout();
+            // 
+            // dgvAuthors
+            // 
+            dgvAuthors.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvAuthors.Dock = DockStyle.Fill;
+            dgvAuthors.Location = new Point(0, 0);
+            dgvAuthors.Name = "dgvAuthors";
+            dgvAuthors.RowHeadersWidth = 62;
+            dgvAuthors.Size = new Size(800, 450);
+            dgvAuthors.TabIndex = 0;
+            // 
+            // frmAuthors
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(dgvAuthors);
+            Name = "frmAuthors";
+            Text = "Authors";
+            Load += frmAuthors_Load;
+            ((System.ComponentModel.ISupportInitialize)dgvAuthors).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private DataGridView dgvAuthors;
+    }
+}
