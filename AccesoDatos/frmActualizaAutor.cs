1629d70630266193cac1062fdb66e5bcd8e7975c
jmot4 <jmota@gtech-services.com>
2025-02-26 15:45:35 -0600

    Se agrega pantalla para actualizacion de informacion de autores.

---
+++ AccesoDatos/frmActualizaAutor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AccesoDatos
+{
+    public partial class frmActualizaAutor : Form
+    {
+        public frmActualizaAutor(string id,string fname,string lname,
+            string phone,string address,string city,string state,string zip,
+            bool contract)
+        {
+            InitializeComponent();
+        }
+
+        private void frmActualizaAutor_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
